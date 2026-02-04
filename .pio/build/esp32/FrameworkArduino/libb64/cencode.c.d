511df1bc8c68d772c324e6a26fccc4308c991dc3
Highpointwebdesign <daryl.lackey@gmail.com>
2026-02-04 11:18:39 -0600

    Remove tracked .pio artifacts

--- .pio/build/esp32/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/Savage\ Cat\ Racing/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/Savage\ Cat\ Racing/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
